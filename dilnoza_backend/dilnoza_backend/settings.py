ed69a416e31ebca6a60f9ae4c3e03d812507810c
Dilnoza971 <dilnozaerkinboyeva899@gmail.com>
2025-05-26 18:33:36 +0500

    Update settings.py

--- dilnoza_backend/dilnoza_backend/settings.py
+++ dilnoza_backend/dilnoza_backend/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    "13.61.180.35"
 ]
 CORS_ALLOW_CREDENTIALS = True
 
